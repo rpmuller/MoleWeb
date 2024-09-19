4411bdbb47dff8f0d2e75549f31e7171cb704cb3
Rick Muller <rpmuller@gmail.com>
2024-09-19 14:47:52 +1000

    Moving toward getting nuclear attraction working

--- pyquante.js
+++ pyquante.js
@@ -39,6 +39,7 @@ suite.add(new TestCase("fact2(3)", fact2(3), 3));
 
 // replacing the old Point with a 3-tuple:
 function distance2(xyz1, xyz2) {
+  console.assert(xyz1.length === xyz2.length);
   let dx = xyz1[0] - xyz2[0];
   let dy = xyz1[1] - xyz2[1];
   let dz = xyz1[2] - xyz2[2];
@@ -239,7 +240,7 @@ function kinetic(alpha1, l1, m1, n1, A, alpha2, l2, m2, n2, B) {
     -0.5 *
     (l2 * (l2 - 1) * overlap(alpha1, l1, m1, n1, A, alpha2, l2 - 2, m2, n2, B) +
       overlap(alpha1, l1, m1, n1, A, alpha2, l2, m2 - 2, n2, B) +
-      overlap(alpha1, l1, m1, n1, A, alpha2, l2, m2, n2-2, B));
+      overlap(alpha1, l1, m1, n1, A, alpha2, l2, m2, n2 - 2, B));
   return term0 + term1 + term2;
 }
 suite.add(
@@ -259,6 +260,148 @@ suite.add(
     ),
     2.953052,
     1e-5
+  )
+);
+
+function array_minus(arr1, arr2) {
+  let diff = Array(arr1.length);
+  for (let i = 0; i < arr1.length; i++) diff[i] = arr1[i] - arr2[i];
+  return diff;
+}
+
+function norm2(arr){
+  let total=0;
+  for (let i=0; i<arr.length; i++) total += arr[i]**2;
+  return total
+}
+
+function nuclear_attraction(alpha1, l1, m1, n1, A, alpha2, l2, m2, n2, B, C) {
+  let gamma = alpha1 + alpha2;
+
+  let P = gaussian_product_center(alpha1, A, alpha2, B);
+  let rab2 = norm2(array_minus(A, B));
+  let rcp2 = norm2(array_minus(C, P));
+
+  let dPA = array_minus(P, A);
+  let dPB = array_minus(P, B);
+  let dPC = array_minus(P, C);
+
+  let Ax = A_array(l1, l2, dPA[0], dPB[0], dPC[0], gamma);
+  let Ay = A_array(m1, m2, dPA[1], dPB[1], dPC[1], gamma);
+  let Az = A_array(n1, n2, dPA[2], dPB[2], dPC[2], gamma);
+
+  let total = 0;
+  for (let I = 0; I < l1 + l2 + 1; I++)
+    for (let J = 0; J < m1 + m2 + 1; J++)
+      for (let K = 0; K < n1 + n2 + 1; K++)
+        total += Ax[I] * Ay[J] * Az[K] ;// * Fgamma(I + J + K, rcp2 * gamma);
+
+  let val =
+    ((-2 * Math.PI) / gamma) * Math.exp((-alpha1 * alpha2 * rab2) / gamma) * total;
+  return val;
+}
+suite.add(new TestCase("nuclear_attraction",nuclear_attraction(1,0,0,0,[0,0,0],1,0,0,0,[0,0,0],[0,0,0]),-3.141593,1e-6));
+
+function A_term(i, r, u, l1, l2, PAx, PBx, CPx, gamma) {
+  return (
+    (Math.pow(-1, i) *
+      binomial_prefactor(i, l1, l2, PAx, PBx) *
+      Math.pow(-1, u) *
+      fact(i) *
+      Math.pow(CPx, i - 2 * r - 2 * u) *
+      Math.pow(0.25 / gamma, r + u)) /
+    fact(r) /
+    fact(u) /
+    fact(i - 2 * r - 2 * u)
+  );
+}
+suite.add(
+  new TestCase(
+    "A_term(0,0,0,0,0,0,0,0,1)",
+    A_term(0, 0, 0, 0, 0, 0, 0, 0, 1),
+    1.0
+  )
+);
+suite.add(
+  new TestCase(
+    "A_term(0,0,0,0,1,1,1,1,1)",
+    A_term(0, 0, 0, 0, 1, 1, 1, 1, 1),
+    1.0
+  )
+);
+suite.add(
+  new TestCase(
+    "A_term(1,0,0,0,1,1,1,1,1)",
+    A_term(1, 0, 0, 0, 1, 1, 1, 1, 1),
+    -1.0
+  )
+);
+suite.add(
+  new TestCase(
+    "A_term(0,0,0,1,1,1,1,1,1)",
+    A_term(0, 0, 0, 1, 1, 1, 1, 1, 1),
+    1.0
+  )
+);
+suite.add(
+  new TestCase(
+    "A_term(1,0,0,1,1,1,1,1,1)",
+    A_term(1, 0, 0, 1, 1, 1, 1, 1, 1),
+    -2.0
+  )
+);
+suite.add(
+  new TestCase(
+    "A_term(2,0,0,1,1,1,1,1,1)",
+    A_term(2, 0, 0, 1, 1, 1, 1, 1, 1),
+    1.0
+  )
+);
+suite.add(
+  new TestCase(
+    "A_term(2,0,1,1,1,1,1,1,1)",
+    A_term(2, 0, 1, 1, 1, 1, 1, 1, 1),
+    -0.5
+  )
+);
+suite.add(
+  new TestCase(
+    "A_term(2,1,0,1,1,1,1,1,1)",
+    A_term(2, 1, 0, 1, 1, 1, 1, 1, 1),
+    0.5
+  )
+);
+
+function A_array(l1, l2, PA, PB, CP, g) {
+  let Imax = l1 + l2 + 1;
+  let A = Array(Imax).fill(0);
+  for (let i = 0; i < Imax; i++) {
+    for (let r = 0; r < Math.floor(i / 2) + 1; r++) {
+      for (let u = 0; u < Math.floor((i - 2 * r) / 2) + 1; u++) {
+        let I = i - 2 * r - u;
+        A[I] += A_term(i, r, u, l1, l2, PA, PB, CP, g);
+      }
+    }
+  }
+  return A;
+}
+
+function sum(arr) {
+  let total = 0;
+  for (let ai of arr) total += ai;
+  return total;
+}
+suite.add(
+  new TestCase("A_array(0,0,0,0,0,1)", A_array(0, 0, 0, 0, 0, 1), [1.0])
+);
+suite.add(
+  new TestCase("A_array(0,1,1,1,1,1)", A_array(0, 1, 1, 1, 1, 1), [1.0, -1.0])
+);
+suite.add(
+  new TestCase(
+    "A_array(1,1,1,1,1,1)",
+    A_array(1, 1, 1, 1, 1, 1),
+    [1.5, -2.5, 1.0]
   )
 );
 
